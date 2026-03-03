6f7836f6331fa2f8f1515f1e3d49c8b7edbf2c32
Arjun Dulam <arjunkumardulam@gmail.com>
2026-03-03 15:18:14 -0500

    vibe coded a debug panel for elegant presentation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,5 @@
-import React, { useState } from "react";
-import Header from "./components/Header";
-import Instructions from "./components/Instructions";
-import GameLayout from "./components/GameLayout";
-import ChallengeControls from "./components/ChallengeControls";
+import DebugPanel from './DebugPanel';
 
 export default function App() {
-  const [showInstructions, setShowInstructions] = useState(true);
-
-  return (
-    <div className="min-h-screen bg-gray-50 text-gray-900">
-      <Header onOpenInstructions={() => setShowInstructions(true)} />
-      
-      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 space-y-6">
-        <GameLayout />
-        <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-4">
-          <ChallengeControls />
-        </div>
-      </main>
-
-      {/* Instructions Modal */}
-      {showInstructions && (
-        <Instructions onClose={() => setShowInstructions(false)} />
-      )}
-    </div>
-  );
+  return <DebugPanel />;
 }
